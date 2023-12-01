5f988db21b96ac25055672f42575342b57216e00
GustavGroenborg <gustavrisagerus@gmail.com>
2023-12-01 17:12:48 +0100

    Finished part 2

--- dec1/src/main/java/Dec1.java
+++ dec1/src/main/java/Dec1.java
@@ -8,30 +8,114 @@ public class Dec1 {
     public static void main(String[] args) {
         String puzzleInput = Dec1.loadPuzzleInput();
 
-        System.out.println("The sum of all calibration values is: "
+        System.out.println("The sum of all calibration values, in part 1, is: "
                 + part1(puzzleInput));
+
+        System.out.println("The sum of all calibration values, in part 2, is: "
+                + part2(puzzleInput));
     }
 
     public static int part1(String puzzleInput) {
         Integer sum = Arrays.stream(puzzleInput.split("\n"))
-                .mapToInt(Dec1::findDigitsIn)
+                .mapToInt(Dec1::findFirstAndLastNumericDigitsIn)
                 .sum();
 
         return sum;
     }
 
-    public static Integer findDigitsIn(String string) {
-        List<String> digitsFound = new ArrayList<>();
-        Pattern digits = Pattern.compile("[1-9]");
+    public static Integer findFirstAndLastNumericDigitsIn(String string) {
+        List<Integer> digitsFound = findAllNumericDigitsIn(string);
+
+        return (digitsFound.getFirst() * 10) + digitsFound.getLast();
+    }
+
+
+    public static List<Integer> findAllNumericDigitsIn(String string) {
+        List<String> unparsedDigits = new ArrayList<>();
+        Pattern digits = Pattern.compile("\\d");
         Matcher matcher = digits.matcher(string);
 
         while (matcher.find())
-            digitsFound.add(matcher.group(0));
+            unparsedDigits.add(matcher.group());
 
-        String unparsedDigits = digitsFound.getFirst() + digitsFound.getLast();
-
-        return Integer.parseInt(unparsedDigits);
+        return unparsedDigits.stream().mapToInt(Integer::parseInt).boxed().toList();
     }
+
+
+    public static int part2(String puzzleInput) {
+        Integer sum = Arrays.stream(puzzleInput.split("\n"))
+                .mapToInt(Dec1::findFirstAndLastDigitsIn)
+                .sum();
+        return sum;
+    }
+
+
+    public static Integer findFirstAndLastDigitsIn(String string) {
+        List<Integer> foundDigits = findAllDigitsIn(string);
+
+        return (foundDigits.getFirst() * 10) + foundDigits.getLast();
+    }
+
+
+    public static List<Integer> findAllDigitsIn(String string) {
+        String unscannedString = string;
+        Pattern writtenDigits = Pattern.compile( "one|two|three|four|five|six|seven|eight|nine");
+        Matcher writtenDigitsMatcher = writtenDigits.matcher(unscannedString);
+
+        while (writtenDigitsMatcher.find()) {
+            String match = writtenDigitsMatcher.group();
+            int matchIndex = unscannedString.indexOf(match);
+            unscannedString = unscannedString.substring(0, matchIndex)
+                    + parseWrittenDigitFrom(match)
+                    + unscannedString.substring(matchIndex + 1);
+            writtenDigitsMatcher = writtenDigits.matcher(unscannedString);
+        }
+
+        Pattern digits = Pattern.compile("\\d");
+        Matcher digitMatcher = digits.matcher(unscannedString);
+        List<String> scannedDigits = new ArrayList<>();
+        while (digitMatcher.find())
+            scannedDigits.add(digitMatcher.group());
+
+        List<Integer> parsedDigits = parseDigitsFrom(scannedDigits);
+        return parsedDigits;
+    }
+
+
+    public static List<Integer> parseDigitsFrom(List<String> unparsedDigits) {
+        List<Integer> parsedDigits = new ArrayList<>();
+
+        for (String unparsedDigit : unparsedDigits) {
+            try {
+                parsedDigits.add(Integer.parseInt(unparsedDigit));
+            } catch (Exception e) {
+                if (e.getClass() == NumberFormatException.class) {
+                    parsedDigits.add(parseWrittenDigitFrom(unparsedDigit));
+                } else {
+                    throw new Error(e.getMessage());
+                }
+            }
+        }
+
+        return parsedDigits;
+    }
+
+
+    /***
+     * @param writtenDigit The written digit.
+     */
+    public static Integer parseWrittenDigitFrom(String writtenDigit) {
+        List<String> writtenDigits = Arrays.asList(
+                "one", "two", "three", "four", "five", "six",
+                "seven", "eight", "nine");
+
+        if (writtenDigits.contains(writtenDigit))
+            return writtenDigits.indexOf(writtenDigit) + 1;
+
+        throw new Error("Could not parse written digit. Got: " + writtenDigit);
+
+    }
+
 
     public static String loadPuzzleInput() {
         String path = System.getProperty("user.home")
--- dec1/src/test/java/Dec1Tests.java
+++ dec1/src/test/java/Dec1Tests.java
@@ -3,11 +3,12 @@ import static org.junit.Assert.*;
 
 import java.util.List;
 import java.util.Arrays;
+import java.util.stream.IntStream;
 
 public class Dec1Tests {
 
     @Test
-    public void test_digitsFound() {
+    public void test_findFirstAndLastNumericDigits() {
         // Given
         String[] input = {
                 "1abc2",
@@ -21,12 +22,179 @@ public class Dec1Tests {
 
         // When
         List<Integer> outputs = Arrays.stream(input)
-                .mapToInt(Dec1::findDigitsIn)
+                .mapToInt(Dec1::findFirstAndLastNumericDigitsIn)
                 .boxed()
                 .toList();
 
         // Then
         for (int i = 0; i < targets.length; i++)
             assertEquals(targets[i], outputs.get(i));
+    }
+
+
+    @Test
+    public void test_findFirstAndLastDigits() {
+        // Given
+        String[] input = {
+                "two1nine",
+                "eightwothree",
+                "abcone2threexyz",
+                "xtwone3four",
+                "4nineeightseven2",
+                "zoneight234",
+                "7pqrstsixteen",
+        };
+        Integer[] targets = {
+                29, 83, 13, 24, 42, 14, 76
+        };
+
+        // When
+        List<Integer> outputs = Arrays.stream(input)
+                .mapToInt(Dec1::findFirstAndLastDigitsIn)
+                .boxed()
+                .toList();
+
+        // Then
+        for (int i = 0; i < targets.length; i++)
+            assertEquals(targets[i], outputs.get(i));
+    }
+
+
+    @Test
+    public void test_findAllDigitsIn() {
+        // Given
+        String input = "1twothree4";
+        Integer[] targets = { 1, 2, 3, 4 };
+
+        // When
+        List<Integer> outputs = Dec1.findAllDigitsIn(input);
+
+        // Then
+        if (targets.length != outputs.size())
+            fail("Unexpected length of outputs."
+                    + " " + "Expected: " + targets.length
+                    + " " + "got: " + outputs.size());
+
+        assertEquals(targets[0], outputs.get(0));
+        assertEquals(targets[1], outputs.get(1));
+        assertEquals(targets[2], outputs.get(2));
+        assertEquals(targets[3], outputs.get(3));
+    }
+
+
+    @Test
+    public void test_parseWrittenDigitAndNumericDigit() {
+        // Given
+        List<String> input = Arrays.asList("1", "two");
+        Integer[] targets = { 1, 2 };
+
+        // When
+        List<Integer> outputs = Dec1.parseDigitsFrom(input);
+
+        if (targets.length != outputs.size())
+            fail("Unexpected length of outputs. Expected: " + targets.length
+                    + " " + "got: " + outputs.size());
+
+        // Then
+        assertEquals(targets[0], outputs.get(0));
+        assertEquals(targets[1], outputs.get(1));
+    }
+
+
+    @Test
+    public void test_FindAllWrittenDigits() {
+        // Given
+        List<String> inputs = Arrays.asList(new String[]{
+                "one", "two", "three", "four", "five", "six", "seven",
+                "eight", "nine",
+        });
+        List<Integer> targets = IntStream.range(1, 10).boxed().toList();
+
+        // When
+        List<Integer> outputs = Dec1.parseDigitsFrom(inputs);
+
+        // Then
+        if (targets.size() != outputs.size())
+            fail("Unexpected size of outputs."
+                    + " " + "Expected: " + targets.size()
+                    + " " + "got: " + outputs.size());
+
+        for (int i = 0; i < targets.size(); i++)
+            assertEquals(targets.get(i), outputs.get(i));
+    }
+
+
+    @Test
+    public void test_part2ExampleInput() {
+        // Given
+        String input = """
+                two1nine
+                eightwothree
+                abcone2threexyz
+                xtwone3four
+                4nineeightseven2
+                zoneight234
+                7pqrstsixteen
+                """;
+        Integer target = 281;
+
+        // When
+        Integer output = Dec1.part2(input);
+
+        // Then
+        assertEquals(target, output);
+    }
+
+
+    @Test
+    public void test_findAllDigitsIn_findsAllNumericDigits() {
+        // Given
+        String input = "123456789";
+        List<Integer> targets = IntStream.range(1,10).boxed().toList();
+
+        // When
+        List<Integer> outputs = Dec1.findAllDigitsIn(input);
+
+        // Then
+        if (targets.size() != outputs.size())
+            fail("Unexpected size of outputs."
+                    + " " + "Expected: " + targets.size()
+                    + " " + "got: " + outputs.size());
+
+        for (int i = 0; i < targets.size(); i++)
+            assertEquals(targets.get(i), outputs.get(i));
+    }
+
+    @Test
+    public void test_findAllDigitsIn_eighthree() {
+        // Given
+        String input = "eighthree";
+        List<Integer> targets = Arrays.asList(8, 3);
+
+        // When
+        List<Integer> outputs = Dec1.findAllDigitsIn(input);
+
+        // Then
+        if (targets.size() != outputs.size())
+            fail("Unexpected size of outputs."
+                    + " " + "Expected: " + targets.size()
+                    + " " + "got: " + outputs.size());
+
+        for (int i = 0; i < targets.size(); i++)
+            assertEquals(targets.get(i), outputs.get(i));
+    }
+
+
+    @Test
+    public void test_stringReplacement() {
+        // Given
+        String input = "foo";
+        String target = "oo";
+
+        // When
+        String output = input.replaceFirst("\\w", "");
+
+        // Then
+        assertEquals(target, output);
     }
 }
