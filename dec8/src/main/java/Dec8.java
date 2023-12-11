107e774f0e713e6776128f537edacbbb03caec5d
GustavGroenborg <gustavrisagerus@gmail.com>
2023-12-11 18:17:49 +0100

    Finished part 2

--- dec8/src/main/java/Dec8.java
+++ dec8/src/main/java/Dec8.java
@@ -3,19 +3,17 @@ import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import com.google.common.collect.Iterables;
+
 public class Dec8 {
     public static void main(String[] args) {
         String pip = getPuzzleInput();
         System.out.println(part1(pip) + " " + "steps are required to reach ZZZ");
+        System.out.println(part2(pip) + " " + "steps have to be taken before all nodes end on Z");
     }
 
     public static int part1(String pip) {
-        Hashtable<String, String[]> map = new Hashtable<>();
-        Arrays.stream(pip.split("\n"))
-                .filter(str -> str.indexOf('=') != -1)
-                .map(str -> str.replace(" = ", " "))
-                .map(str -> str.replaceAll("[(,)]", "").split("\\s"))
-                .forEach(arr -> map.put(arr[0], new String[]{ arr[1], arr[2] }));
+        var map = generateHashtableFrom(pip);
         var node = "AAA";
         var iterations = 0;
         List<Character> instructions = getInstructionsFrom(pip);
@@ -30,12 +28,71 @@ public class Dec8 {
         return iterations;
     }
 
-    private static List<Character> getInstructionsFrom(String string) {
+    public static long part2(String pip) {
+        var map = generateHashtableFrom(pip);
+        Iterable<Character> instructionsCycle = Iterables.cycle(getInstructionsFrom(pip));
+        var nodes = findNodesEndingIn('A', pip);
+        var intervals = new ArrayList<Integer>();
+
+        while (!nodes.isEmpty()) {
+            var instructions = instructionsCycle.iterator();
+            int iterations = 0;
+            var node = nodes.removeFirst();
+
+            while (!node.matches("[\\w]{2}Z") && instructions.hasNext()) {
+                var instruction = instructions.next();
+                iterations++;
+
+                node = map.get(node)[instruction == 'L' ? 0 : 1];
+            }
+            intervals.add(iterations);
+        }
+
+        long iterations = (long) intervals.removeFirst();
+        while (!intervals.isEmpty()) {
+            iterations = lcm(iterations, intervals.removeFirst());
+        }
+
+        return iterations;
+    }
+
+
+    private static long lcm(long a, long b) {
+        long high = Math.max(a, b);
+        long low = Math.min(a, b);
+        long lcm = high;
+        while (lcm % low != 0)
+            lcm += high;
+        return lcm;
+    }
+
+    private static Hashtable<String, String[]> generateHashtableFrom(String string) {
+        Hashtable<String, String[]> map = new Hashtable<>();
+        Arrays.stream(string.split("\n"))
+                .filter(str -> str.indexOf('=') != -1)
+                .map(str -> str.replace(" = ", " "))
+                .map(str -> str.replaceAll("[(,)]", "").split("\\s"))
+                .forEach(arr -> map.put(arr[0], new String[]{ arr[1], arr[2] }));
+        return map;
+    }
+
+
+    private static List<String> findNodesEndingIn(char c, String string) {
+        List<String> nodes = new ArrayList<>();
+        String regex = String.format("[\\w]{2}%c", c);
+        Matcher matcher = Pattern.compile(regex).matcher(string);
+        while (matcher.find())
+            nodes.add(matcher.group());
+
+        return nodes;
+    }
+
+    private static ArrayList<Character> getInstructionsFrom(String string) {
         Matcher matcher = Pattern.compile("[RL]+").matcher(string);
         if (!matcher.find())
             throw new Error("Could not find instructions in: " + string);
 
-        return matcher.group().chars().mapToObj(i -> Character.valueOf((char) i)).toList();
+        return new ArrayList<>(matcher.group().chars().mapToObj(i -> Character.valueOf((char) i)).toList());
     }
 
     public static String getPuzzleInput() {
